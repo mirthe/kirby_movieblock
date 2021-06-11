d07873a6786b7935737b841e3db7962bd0348ff7
Mirthe Valentijn <mirthe@aanzee.nl>
2021-06-11 16:37:01 +0200

    Add top billed cast

--- index.php
+++ index.php
@@ -17,9 +17,15 @@
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
                 $rawdata = curl_exec($ch);
                 curl_close($ch);
-                
                 $movieinfo = json_decode($rawdata,true);
                 // print_r($movieinfo); exit(); 
+
+                $url = "https://api.themoviedb.org/3/movie/". $tmdbid ."/credits?api_key=" . $api_key;
+                $ch = curl_init($url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                $rawdata_credits = curl_exec($ch);
+                curl_close($ch);
+                $credits = json_decode($rawdata_credits,true);
                 
                 $mijnoutput = '<div class="well" style="overflow: auto;">';
                 $mijnoutput .= '<img src="https://www.themoviedb.org/t/p/w200/'.$movieinfo['poster_path'].'" alt="" class="floatleft" style="margin-right: 1rem;">';
@@ -28,12 +34,22 @@
                 $mijnoutput .= '<p><em>'.$movieinfo['tagline']."</em></p>";
                 $mijnoutput .= '<p>'.$movieinfo['overview']."</p>";
 
+                $i = 0;
+                $mijnoutput .= "<ul class=\"cast\">";
+                foreach ($credits['cast'] as $genre) {
+                    $mijnoutput .= '<li>'. $genre['name'] . "</li>";
+                    if (++$i == 5) break;
+                }
+                $mijnoutput .= "</ul>";
+
                 $mijnoutput .= "<ul class=\"genres\">";
                 foreach ($movieinfo['genres'] as $genre) {
                     $mijnoutput .= '<li>'. $genre['name'] . "</li>";
                 }
                 $mijnoutput .= "</ul>";
 
+                $mijnoutput .= '</div>';
+               
                 return $mijnoutput;
             }
         ]
