c0321b5a3d2300d5618c6a770939fc315c1e6f9a
Mirthe Valentijn <me@mirthe.org>
2026-05-27 21:00:25 +0200

    Kirby caching

--- index.php
+++ index.php
@@ -8,46 +8,68 @@
                 'tmdb'
             ],
             'html' => function($tag) {
-                
                 $tmdbid = $tag->tmdb;
                 $api_key = option('themoviedb.apiKey');
-               
-                $url = "https://api.themoviedb.org/3/movie/". $tmdbid ."?api_key=" . $api_key;
-                $ch = curl_init($url);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($ch, CURLOPT_USERAGENT, kirby()->site()->title());
-                $rawdata = curl_exec($ch);
-                curl_close($ch);
-                $movieinfo = json_decode($rawdata,true);
-                // print_r($movieinfo); exit(); 
 
-                $url = "https://api.themoviedb.org/3/movie/". $tmdbid ."/credits?api_key=" . $api_key;
-                $ch = curl_init($url);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($ch, CURLOPT_USERAGENT, kirby()->site()->title());
-                $rawdata_credits = curl_exec($ch);
-                curl_close($ch);
-                $credits = json_decode($rawdata_credits,true);
-                
+                $cache = kirby()->cache('mirthe.movieblock');
+                $cacheKey = 'tmdb-' . $tmdbid;
+                $movieData = $cache->get($cacheKey);
+
+                if ($movieData === null) {
+                    $movieUrl = "https://api.themoviedb.org/3/movie/" . $tmdbid . "?api_key=" . $api_key;
+                    $ch = curl_init($movieUrl);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt($ch, CURLOPT_USERAGENT, kirby()->site()->title());
+                    $rawMovie = curl_exec($ch);
+                    curl_close($ch);
+
+                    $creditsUrl = "https://api.themoviedb.org/3/movie/" . $tmdbid . "/credits?api_key=" . $api_key;
+                    $ch = curl_init($creditsUrl);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt($ch, CURLOPT_USERAGENT, kirby()->site()->title());
+                    $rawCredits = curl_exec($ch);
+                    curl_close($ch);
+
+                    $movieData = [
+                        'movie'   => json_decode($rawMovie, true),
+                        'credits' => json_decode($rawCredits, true)
+                    ];
+
+                    $cache->set($cacheKey, $movieData, 604800);
+                }
+
+                $movieinfo = $movieData['movie'] ?? null;
+                $credits = $movieData['credits'] ?? null;
+
+                if (empty($movieinfo) || !is_array($movieinfo) || isset($movieinfo['status_code'])) {
+                    return '<div class="well"><div class="well-body">Film niet gevonden</div></div>';
+                }
+
+                $poster = isset($movieinfo['poster_path']) ? 'https://www.themoviedb.org/t/p/w200/'.$movieinfo['poster_path'] : '';
                 $mijnoutput = '<div class="well">';
-                $mijnoutput .= '<div class="well-img"><img src="https://www.themoviedb.org/t/p/w200/'.$movieinfo['poster_path'].'" alt="" class="img" width="200" height="300"></div>';
+                if ($poster !== '') {
+                    $mijnoutput .= '<div class="well-img"><img src="'.$poster.'" alt="" class="img" width="200" height="300"></div>';
+                }
                 $mijnoutput .= '<div class="well-body">';
-                // $mijnoutput .= '<a href="https://www.imdb.com/title/'.$movieinfo['imdb_id'].'" class="floatright" title="Bekijken op IMDb">IMDb</a>';
-                $mijnoutput .= '<p><a href="https://www.themoviedb.org/movie/'.$movieinfo['id'].'">'.$movieinfo['original_title']."</a><br>". $movieinfo['release_date']."</p>";
-                $mijnoutput .= '<p><em>'.$movieinfo['tagline']."</em></p>";
-                $mijnoutput .= '<p>'.mb_strimwidth($movieinfo['overview'],0,300, '&#8230;')."</p>";
+                $mijnoutput .= '<p><a href="https://www.themoviedb.org/movie/'.$movieinfo['id'].'">'.($movieinfo['original_title'] ?? '')."</a><br>".($movieinfo['release_date'] ?? '')."</p>";
+                $mijnoutput .= '<p><em>'.($movieinfo['tagline'] ?? '')."</em></p>";
+                $mijnoutput .= '<p>'.mb_strimwidth($movieinfo['overview'] ?? '', 0, 300, '&#8230;')."</p>";
 
-                $i = 0;
                 $mijnoutput .= "<ul class=\"cast\">";
-                foreach ($credits['cast'] as $genre) {
-                    $mijnoutput .= '<li>'. $genre['name'] . "</li>";
-                    if (++$i == 5) break;
+                if (!empty($credits['cast']) && is_array($credits['cast'])) {
+                    $i = 0;
+                    foreach ($credits['cast'] as $castMember) {
+                        $mijnoutput .= '<li>'.($castMember['name'] ?? '')."</li>";
+                        if (++$i === 5) break;
+                    }
                 }
                 $mijnoutput .= "</ul>";
 
                 $mijnoutput .= "<ul class=\"genres\">";
-                foreach ($movieinfo['genres'] as $genre) {
-                    $mijnoutput .= '<li>'. $genre['name'] . "</li>";
+                if (!empty($movieinfo['genres']) && is_array($movieinfo['genres'])) {
+                    foreach ($movieinfo['genres'] as $genre) {
+                        $mijnoutput .= '<li>'.($genre['name'] ?? '')."</li>";
+                    }
                 }
                 $mijnoutput .= "</ul>";
 
